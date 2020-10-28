6e56c26aa3a5e9572c4d11c564919a1c966e7b9f
alissonrms <armelquiades2000@gmail.com>
2020-10-28 20:21:56 -0300

    criação do módulo de clientes e rotas

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+//localhost:4200
+const routes: Routes = [
+  {
+    path: 'clientes',
+    loadChildren: () => import('./clients/clients.module').then(m => m.ClientsModule)
+  },
+
+  {
+    path: '',
+    redirectTo: 'clientes',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
